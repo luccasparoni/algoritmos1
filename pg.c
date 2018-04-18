225dcde4183ff30923cb5efc0ea3d5a1a7c89df0
Luccas Paroni <luccas_paroni@hotmail.com>
2018-04-18 13:27:49 -0300

    Add files via upload

--- pg.c
+++ pg.c
@@ -1,47 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ve_se_a_razao_eh_igual(int* vetor, int tamanho, float razao, int onde_ta){
+int razaoEhIgual(float* vetor, int tam, float razao, int onde_ta){
 
-	if(onde_ta == 1){
-		razao = vetor[0]/vetor[1];
-		return ve_se_a_razao_eh_igual(vetor, tamanho, razao, 2);
-	}
-	if(onde_ta = tamanho){
-		return 1;
-	}
-	float nova_razao = vetor[onde_ta-1] / vetor[onde_ta];
-	if(nova_razao == razao){
-		ve_se_a_razao_eh_igual(vetor, tamanho, razao, onde_ta+1);
-	}
-	else if(nova_razao != razao){
-		return 0;
-	}
+	if(onde_ta == tam) return 1;
+
+	if((vetor[onde_ta]) / (vetor[onde_ta-1]) == razao)
+		return razaoEhIgual(vetor, tam, razao, onde_ta+1);
+
+	return 0;
 }
 
 void main(){
-	int tamanho=0;
 
+	int tam = 0;
 	int input = 1;
-	int* vetor = (int*) malloc((1)*sizeof(int));
+	float* vetor = (float*) malloc(sizeof(float));
 
 	while(1){
-		vetor = (int*) realloc(vetor,(tamanho+1)*sizeof(int));
+		vetor = (float*) realloc(vetor,(tam+1)*sizeof(float));
 		scanf("%d", &input);
-		if(input == 0){
-			break;
-		}
-		vetor[tamanho] = input;
-		printf("%d", vetor[tamanho]);
+		if(input == 0)break;
 
-		tamanho++;
+		vetor[tam] = input;
+		tam++;
 	}
 
-	float razao;
-	int resposta = ve_se_a_razao_eh_igual(vetor, tamanho, razao, 1);
-	if(resposta == 1){
-		printf("eh pg");
-	}else{
-		printf("nao eh pg");
-	}
+	razaoEhIgual(vetor, tam, vetor[1]/vetor[0], 1) ? printf("eh pg"):printf("nao eh pg");
+
 }
--- soma.c
+++ soma.c
@@ -2,16 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
+double vetor[5]  = {1.0,2.0,5.0,8.0,16.0};
 void ve_se_ta_Certo(double a1, double q, int inicio, int fim){
-	if(fim == inicio){
+	printf("teste4\n");
+
+	if(fim == inicio || fim == inicio+1){
 		double valor = pow(q, fim-1);
-		printf("o valor que falta é %f", a1*valor);
+		printf("%f\n",a1*valor);
+		if(vetor[fim] == a1*valor){
+			printf("fim %d\n", fim);
+			printf("o valor que falta é %f", a1*valor);
+		}else{
+			printf("não falta nada\n");
+		}
+		return;
 	}
-	double novoq = pow(q, fim/2);
-	if(vetor[fim/2] == a1*novoq){
-		ve_se_ta_Certo(a1, q, fim/2, fim);
+	printf("teste4\n");
+	double novoq = pow(q, (fim+1)/2);
+	printf("%f\n", novoq);
+	if(vetor[fim/2]+1 == a1*novoq){
+		printf("chegou aqui\n");
+		ve_se_ta_Certo(a1, q, (fim+1)/2, fim);
 	}else{
-		ve_se_ta_Certo(a1, q, inicio, fim/2);
+		printf("chegou aqui2\n");
+		ve_se_ta_Certo(a1, q, inicio, (fim+1)/2);
 	}
 }
 
@@ -22,15 +36,19 @@ int main(){
 
 	double q, a1;
 
-	a1 = vetor[0]
+	a1 = vetor[0];
 	if(vetor[1]/vetor[0] == vetor[2]/vetor[1]){
 		q = vetor[1]/vetor[0];
+		printf("teste1\n");
 	}else if(vetor[1]/vetor[0] > vetor[2]/vetor[1]){
+		printf("teste2\n");
 		q = vetor[2]/vetor[1];
-		printf("o termo que falta é ", a1*q);
+		printf("o termo que falta é %f ", a1*q);
 	}else{
 		q = vetor[1]/vetor[0];
+		printf("teste3\n");
+
 	}
 
-	ve_se_ta_Certo(a1, q, 0, n);
+	ve_se_ta_Certo(a1, q, 0, 4);
 }
