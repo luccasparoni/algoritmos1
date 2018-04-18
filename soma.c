aa572548d00a4572ae83eed92474a14565c7ebc8
Luccas Paroni <luccas_paroni@hotmail.com>
2018-04-18 19:59:13 -0300

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double vetor[5]  = {32,16,8,2,1};
+void ve_se_ta_Certo(double a1, double q, int inicio, int fim){
+
+	if(fim == inicio){
+
+		if(fim > 0 && vetor[fim]/vetor[fim-1] != q){
+			printf("falta o %f\n", a1*pow(q, fim));
+		}else if(fim > 0 && vetor[fim+1]/vetor[fim] !=q){
+			printf("falta o %f\n", a1*pow(q, fim+1));
+		}
+		return;
+	}else{
+		if(vetor[(fim-inicio)/2] == a1*pow(q, (fim+1)/2)){
+			ve_se_ta_Certo(a1, q, inicio+(fim-inicio+2)/2, fim);
+		}else{
+			ve_se_ta_Certo(a1, q, inicio, inicio+(fim-inicio)/2);
+		}
+	}
+}
+
+
+int main(){
+
+	//acha a raiz q e o termo inicial a1;
+
+	double q, a1;
+
+	a1 = vetor[0];
+	if(vetor[1]/vetor[0] == vetor[2]/vetor[1]){
+		q = vetor[1]/vetor[0];
+	}else if(vetor[1]/vetor[0] > vetor[2]/vetor[1] && vetor[1]/vetor[0] > 1){
+		q = vetor[2]/vetor[1];
+		printf("o termo que falta é %f ", a1*q);
+	}else if(vetor[1]/vetor[0] > vetor[2]/vetor[1] && vetor[1]/vetor[0] < 1){
+		q = vetor[2]/vetor[1];
+		printf("o termo que falta é %f\n", a1*q );
+	}else{
+		q = vetor[1]/vetor[0];
+
+	}
+
+	ve_se_ta_Certo(a1, q, 0, 4);
+}
